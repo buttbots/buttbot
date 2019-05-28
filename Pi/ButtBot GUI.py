9f5451d0a1f44e0e1acbcc3260fa7f781cfd3cd6
Photoeffect <elfyorsch@gmail.com>
2019-05-28 11:49:20 +0200

    implemented webcamstream with the

--- Pi/ButtBot GUI.py
+++ Pi/ButtBot GUI.py
@@ -1,13 +1,15 @@
 from __future__ import print_function
 import tkinter as tk
 import serial
-from PIL import Image
-from PIL import ImageTk
+import PIL
+from PIL import Image,ImageTk
 import threading
 import datetime
+import pytesseract
 import imutils
 import cv2
 import os
+
 
 """
 ------------------------------------------
@@ -39,28 +41,36 @@ BUTTBOT GUI
 ------------------------------------------
 """
 root = tk.Tk()
+root.resizable(height = False, width = False)
 
-HEIGHT = 600
-WIDTH = 800
+HEIGHT = 800
+WIDTH = 1600
+
+# get cam frames
+camheight = 800
+camwidth = 600
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, camwidth)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, camheight)
 
 
-# Hintergrund 
+# background
 canvas = tk.Canvas(root, height = HEIGHT, width = WIDTH)
 canvas.pack()
 
-# oranger Hintergrund
+# orange background
 background = tk.Frame(root, bg = "#e67300" )
 background.place(relheight = 1, relwidth = 1)
 
-# BUTTBOT Überschrift
+# BUTTBOT Label
 label = tk.Label(root, text = "BUTTBOT", bg = "#994d00", font =("IBM Plex",18))
 label.place(anchor = "n", height = 50, width = 200, relx = 0.5, rely = 0.01)
 
-# grauer Frame
+# grey frame
 cordframe = tk.Frame(root, bg = "#994d00")
 cordframe.place(relheight = 0.8, relwidth = 0.8, relx = 0.1, rely = 0.1)
 
-# X-Koordinate
+# X-coordinate
 xCordLabel = tk.Label(cordframe, text = "X-Wert", bg = "#994d00")
 xCordLabel.place(anchor = "n", relx = 0.2, y = 5)
 
@@ -68,7 +78,7 @@ X = tk.StringVar()
 xCord = tk.Entry(cordframe, bd = 1, bg = "gray", textvariable = X)
 xCord.place(anchor = "n", relx = 0.2, y = 25)
 
-# Y-Koordinate
+# Y-coordinate
 yCordLabel = tk.Label(cordframe, text = "Y-Wert", bg = "#994d00")
 yCordLabel.place(anchor = "n", relx = 0.2, y = 55)
 
@@ -76,13 +86,29 @@ Y = tk.StringVar()
 yCord = tk.Entry(cordframe, bd = 1, bg = "gray", textvariable = Y)
 yCord.place(anchor = "n", relx = 0.2, y = 75)
 
-# Knopf
+# "GO!" Button
 def getxy():
     print(xCord.get(),yCord.get())
 
 button = tk.Button(cordframe, command = getxy, height = 1, width = 16, bg = "#995c00", activebackground = "#b36b00", activeforeground = "#ffffff", text = "Go!", cursor = "target")
 button.place(anchor = "n", relx = 0.2, y = 115)
 
+# CAMSTREAM
+camlabel = tk.Label(cordframe)
+camlabel.place(anchor = "nw", height = 400, width = 600, x = 500, y = 10)
+
+def showframe():
+	_, frame = cap.read()
+	frame = cv2.flip(frame, 1)
+	cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+	img = PIL.Image.fromarray(cv2image)
+	cam = ImageTk.PhotoImage(image=img)
+	camlabel.cam = cam
+	camlabel.configure(image=cam)
+	camlabel.after(10, showframe)
+	root.update
+
+showframe()
 
 root.mainloop()
 """
